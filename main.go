7c2c5fddcc7e5b2fab56173e44bf2ba160f97acb
ktalele-rescale <ktalele+github@rescale.com>
2023-09-09 17:24:33 -0400

    - generate

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 
+	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
-	fmt.Println("Starting")
 	n := maelstrom.NewNode()
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
@@ -20,6 +19,19 @@ func main() {
 		}
 
 		body["type"] = "echo_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = uuid.NewString()
 
 		return n.Reply(msg, body)
 	})
